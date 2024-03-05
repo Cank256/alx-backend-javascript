daec8914a44db15c3c574835f83e1063a21c1211
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-05 12:10:34 +0300

    Lets go advanced

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+export default function createIteratorObject(report) {
+	// This is a generator function that yields every employee from every department
+	return (function* () {
+		for (const department of Object.keys(report.allEmployees)) {
+			for (const employee of report.allEmployees[department]) {
+				yield employee;
+			}
+		}
+	})();
+}
