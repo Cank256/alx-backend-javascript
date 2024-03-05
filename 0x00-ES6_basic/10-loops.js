0a2ce455a978e5aaca646752704e66c4e16195a2
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-05 12:08:15 +0300

    COntinuation

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+	const result = [];
+	for (const value of array) {
+		result.push(appendString + value);
+	}
+	return result;
+}
