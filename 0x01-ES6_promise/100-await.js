db59ca0fdad88668b1b9b5dad5e7ab85a57c7ecd
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-07 12:03:19 +0300

    Work on the lint

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,11 +2,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export async function asyncUploadUser() {
-    try {
-        const photo = await uploadPhoto();
-        const user = await createUser();
-        return { photo, user };
-    } catch (error) {
-        return { photo: null, user: null };
-    }
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,13 +3,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName)
-    ]).then((results) => {
-        return results.map(result => ({
-            status: result.status,
-            value: result.reason || result.value
-        }));
-    });
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.reason || result.value,
+  })));
 }
