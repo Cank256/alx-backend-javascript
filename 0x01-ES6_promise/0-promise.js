cb528d459315d5ec1c1c558bde205914d4f92cdb
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-07 12:08:58 +0300

    Obey link

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // The function currently does not do anything in particular and does not resolve nor reject
-  });
+  return new Promise(() => {});
 }
