7d9cdf88f4843f00ab1dab36920a59e3ff060734
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-08 20:00:46 +0300

    The car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,13 +1,19 @@
 #!/usr/bin/node
+const uniqueIdSymbol = Symbol('unique identifier for cloning');
+
 export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
+    this[uniqueIdSymbol] = Symbol();
   }
 
-  // Method to clone a car
   cloneCar() {
-    return new this.constructor();
+    const cloned = new Car(this._brand, this._motor, this._color);
+    // Preserve uniqueIds to avoid accidental equality 
+    cloned[uniqueIdSymbol] = this[uniqueIdSymbol]; 
+    return cloned;
   }
 }
+
