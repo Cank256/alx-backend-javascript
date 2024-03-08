52687225e0d6e16e3922063518b7d6f3e893c645
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-08 19:45:53 +0300

    pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,38 +1,12 @@
 #!/usr/bin/node
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
-// 4-pricing.js file
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof amount !== 'number') throw TypeError('Amount must be a number');
+    if (!(currency instanceof Currency)) throw TypeError('Currency must be an instance of Currency');
+
     this._amount = amount;
     this._currency = currency;
-  }
-
-  // Getter and setter for amount
-  get amount() {
-    return this._amount;
-  }
-
-  set amount(newAmount) {
-    this._amount = newAmount;
-  }
-
-  // Getter and setter for currency
-  get currency() {
-    return this._currency;
-  }
-
-  set currency(newCurrency) {
-    this._currency = newCurrency;
-  }
-
-  // Method to display full price
-  displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
-  }
-
-  // Static method to convert price
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
   }
 }
