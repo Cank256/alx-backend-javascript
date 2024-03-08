6d19b8ae4b9284a037cf56f1c000621a5a57fd3c
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-03-08 20:09:03 +0300

    evcar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
-// 100-evcar.js
-import Car from './10-car.js'; 
+import Car from './10-car';
 
+// 100-evcar.js file
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
+  // Override the cloneCar method
   cloneCar() {
-    // Return a generic 'Car' instead of an 'EVCar'
-    const newCar = super.cloneCar();
-    return newCar;
+    return new Car(); // Ensures cloning as Car instance, not EVCar
   }
 }
