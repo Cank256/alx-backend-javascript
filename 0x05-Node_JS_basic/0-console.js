439f112ac49e435be88ce320b9a718b242c4e31c
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-04-30 12:14:37 +0300

    NodeJs basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * Displays the message in STDOUT (standard output).
+ * @param {string} message - The message to be displayed.
+ */
+function displayMessage (message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
