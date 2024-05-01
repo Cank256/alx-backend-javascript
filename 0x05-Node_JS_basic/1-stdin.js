de7aabd7fb28595532732468ddace23bff08d66e
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-01 08:54:23 +0300

    NodeJs basics 1 update 2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 // Display a welcome message and prompt the user to input their name
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+// Listen for data input from the user
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit(0);
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
