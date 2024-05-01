84dc0ab0c2dedfcd9f5d6a14478d3281df249714
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-01 08:38:56 +0300

    NodeJs basics 1 update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 
+// Display a welcome message and prompt the user to input their name
 process.stdout.write('Welcome to Holberton School, what is your name?\n')
 
+// Listen for data input from the user
 process.stdin.on('data', (data) => {
+  // Convert the input data to a string and remove any leading or trailing whitespace
   let name = data.toString().trim()
+
+  // Display the user's name
   process.stdout.write(`Your name is: ${name}\n`)
+  
+  // Display a closing message and exit the process
   process.stdout.write('This important software is now closing\n')
   process.exit()
 })
