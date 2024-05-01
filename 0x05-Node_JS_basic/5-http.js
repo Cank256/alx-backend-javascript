200a1359284846c9a0adfcdd7fb4e9ed6fcdb8c4
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-01 15:37:14 +0300

    NodeJs basics 3 lint

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,19 +3,17 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-  if(req.url === '/') {
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if(req.url === '/students') {
-
+  } else if (req.url === '/students') {
     countStudents('database.csv')
-    .then((students) => {
-        console.log(students)
+      .then((students) => {
+        console.log(students);
         res.end(`This is the list of our students\n${students}`);
-      }
-    ).catch((err) => {
-      res.end(`This is the list of our students\n${err.message}`);
-    });
+      }).catch((err) => {
+        res.end(`This is the list of our students\n${err.message}`);
+      });
   }
 }).listen(1245);
 
