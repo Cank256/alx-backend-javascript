e8a842dd21ed6a8a6801d23c0f891d2cdf53a92b
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-01 08:35:34 +0300

    NodeJs basics 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
+
+process.stdin.on('data', (data) => {
+  let name = data.toString().trim()
+  process.stdout.write(`Your name is: ${name}\n`)
+  process.stdout.write('This important software is now closing\n')
+  process.exit()
+})
