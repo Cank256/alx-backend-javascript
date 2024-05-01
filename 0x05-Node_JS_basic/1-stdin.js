96a1157420f5ee56e79e3aed577a6c1b4aa1aeeb
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-01 08:47:53 +0300

    NodeJs basics 1 update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // Listen for data input from the user
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+});
 
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
+// Listen for the close event
+process.on('close', () => {
+  process.stdout.write('This important software is closing\n');
 });
