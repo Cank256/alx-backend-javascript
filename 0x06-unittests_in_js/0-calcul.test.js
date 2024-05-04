73a792e0fb4cf3785cd9eb594ea674e694c5bb27
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-04 17:35:59 +0300

    seperate suits

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,8 +5,23 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function() {
     it('should return the sum of two rounded numbers', function() {
         assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1.2, 3), 4);
+    });
+});
+
+describe('calculateNumber', function() {
+    it('for first number rounded', function() {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+});
+
+describe('calculateNumber', function() {
+    it('for second number rounded', function() {
+        assert.strictEqual(calculateNumber(1.2, 3), 4);
+    });
+});
+
+describe('calculateNumber', function() {
+    it('for both numbers rounded', function() {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
 });
