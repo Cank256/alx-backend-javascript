99c236ca99b9dfbb2cc88a2af10ec463d069a2ec
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-04 10:28:08 +0300

    second number rounded

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,7 +6,7 @@ describe('calculateNumber', function() {
     it('should return the sum of two rounded numbers', function() {
         assert.strictEqual(calculateNumber(1, 3), 4);
         assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3), 4);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
