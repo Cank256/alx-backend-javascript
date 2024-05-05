8635e5f0e41ca2fa5e1950524f5e594a041a7d23
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-05-05 08:11:47 +0300

    update checks 1

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,6 +1,21 @@
 const request = require('request');
 const { expect } = require('chai');
-const server = require('./api');
+
+describe('Index page', function() {
+  it('should return correct status code', function(done) {
+      request.get('http://localhost:7865', (error, response, body) => {
+          expect(response.statusCode).to.equal(200);
+          done();
+      });
+  });
+
+  it('should return correct result', function(done) {
+      request.get('http://localhost:7865', (error, response, body) => {
+          expect(body).to.equal('Welcome to the payment system');
+          done();
+      });
+  });
+});
 
 describe('Cart page', () => {
   it('Correct status code when :id is a number?', (done) => {
